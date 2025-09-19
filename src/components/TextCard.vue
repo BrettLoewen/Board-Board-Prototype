ee0fcfaf536790e85dc65fd74af20cf1cc3888ec
Brett Loewen <brettzky007@gmail.com>
2025-09-19 16:05:56 -0500

    Card grows to fit text

---
+++ src/components/TextCard.vue
@@ -0,0 +1,115 @@
+<script setup>
+import { reactive, onMounted, useTemplateRef } from "vue";
+import interact from "interactjs";
+
+// State
+const cards = reactive([
+  { id: 1, text: "Card A", x: 100, y: -100, width: 150, height: 100 },
+  { id: 2, text: "Card B", x: 400, y: -200, width: 150, height: 100 },
+]);
+
+// Resize the card to fit text (prevent text from overflowing by making the card taller)
+function autoResize(event) {
+  // Get the card element that is the parent of this textarea element
+  const el = event.target;
+  const card = cards.find((c) => el.parentElement.id == c.id);
+  if (!card) {
+    console.log("No card found");
+    return;
+  }
+
+  // Get the padding between the textarea and card to make the measurements accurate
+  var padding = parseFloat(window.getComputedStyle(el.parentElement).paddingTop);
+
+  // If the text has made the textarea too tall for the card
+  if (el.scrollHeight > card.height - padding * 2) {
+    // Calculate the new height for the card using the scroll height, padding, and an extra bit to prevent the scroll bar from appearing
+    var newHeight = el.scrollHeight + padding * 2 + 2;
+
+    card.height = newHeight;
+  }
+}
+
+// Setup interact.js
+onMounted(() => {
+  interact(".card")
+    .draggable({
+      listeners: {
+        move(event) {
+          const el = event.target;
+          const card = cards.find((c) => el.id == c.id);
+          if (!card) {
+            console.log("No card found");
+            return;
+          }
+
+          card.x += event.dx;
+          card.y += event.dy;
+        },
+      },
+      inertia: false,
+    })
+    .resizable({
+      edges: { top: true, left: true, bottom: true, right: true },
+      invert: "reposition",
+      listeners: {
+        move(event) {
+          const el = event.target;
+          const card = cards.find((c) => el.id == c.id);
+          if (!card) {
+            console.log("No card found");
+            return;
+          }
+
+          card.width = event.rect.width;
+          card.height = event.rect.height;
+          card.x += event.deltaRect.left;
+          card.y += event.deltaRect.top;
+        },
+      },
+    });
+});
+</script>
+
+<template>
+  <div
+    v-for="card in cards"
+    :key="card.id"
+    :id="card.id"
+    class="card"
+    :style="{
+      transform: `translate(${card.x}px, ${card.y}px)`,
+      width: card.width + 'px',
+      height: card.height + 'px',
+    }"
+    @input="autoResize"
+  >
+    <textarea class="cardTextarea" placeholder="Type something..."></textarea>
+  </div>
+</template>
+
+<style>
+.card {
+  display: flex;
+  position: absolute;
+  background: white;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 8px;
+  user-select: none;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+.card:active {
+  cursor: grabbing;
+}
+
+.cardTextarea {
+  flex: 1;
+  border-radius: 5px;
+  color: black;
+  resize: none;
+}
+.cardTextarea:active {
+  cursor: grabbing;
+}
+</style>
